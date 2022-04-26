b360653fc8c96cfbb61a52d5fb9ffa8fb5956de3
mihail <mihailname2017@gmail.com>
2022-04-26 15:54:58 +0800

    add chart

--- pages/lab3/lab3.js
+++ pages/lab3/lab3.js
@@ -1,15 +1,55 @@
+var ctx = document.querySelector("#myChart").getContext("2d");
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {},
+    options: {
+        tooltips: {
+            enabled: false,
+        },
+        legend: {
+            display: false
+          }
+      }
+});
+
 function chartt(){
-    //F  ⟂
+     //F  ⟂ X
     var Pr1d1 = document.getElementById('r1d1').value;
     var Pr1d2 = document.getElementById('r1d2').value;
     var Pr1d3 = document.getElementById('r1d3').value;
     var Pr1d4 = document.getElementById('r1d4').value;
     
-    //F  тр
+     //F  тр Y
     var Pr2d1 = document.getElementById('r2d1').value;
     var Pr2d2 = document.getElementById('r2d2').value;
     var Pr2d3 = document.getElementById('r2d3').value;
     var Pr2d4 = document.getElementById('r2d4').value;
 
-    alert(Pr2d3) 
+    // alert(Pr2d3) 
+    var ctx = document.querySelector("#myChart").getContext("2d");
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [0, Pr1d1, Pr1d2 ,Pr1d3, Pr1d4 ],
+            datasets: [{
+                label: "students",
+                data: [0, Pr2d1, Pr2d2, Pr2d3, Pr2d4],
+                fill: true,
+                backgroundColor: [
+                    'transparent'
+                ],
+                borderColor: 'rgb(99, 27, 103)'
+            }]
+        },
+        options: {
+            tooltips: {
+                enabled: false,
+            },
+            legend: {
+                display: false
+              }
+          }
+    });
 }
